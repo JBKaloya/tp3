accc68069f8e2e4c788a4052f68885b06c7bb7ac
Jean-Baptiste Kaloya <me@jbkaloya.fr>
2019-02-07 22:51:38 +0100

    Component declaration  errors 2

---
+++ fbliteece/components/Profil.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+  class Profil extends Component {
+
+    render() {
+
+
+      return(
+          <div className="profilcontainer">
+          //A completer<
+          </div
+      );
+        
+        
+   }
+ }
+
+  export default Profil; 
